1fc3de4a5bcdb70cada861b380537104989e923f
Andrew Stagg <andrewstagg@Andrews-MacBook-Pro.local>
2021-03-25 16:43:30 -0500

    Fix format on dropdown

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,18 +1,18 @@
 const DropDown = ({ name, data, currerntValue, onChange }) => {
-    return (
-        <div>
-            <span>{`${name}`}: </span>
-            <select value={currerntValue} name={`${name}`} onChange={onChange}>
-                {
-                    data.map((value) => {
-                        return (
-                            <option value={value}>{value}</option>
-                        )
-                    })
-                }
-            </select>
-        </div>
-    );
+  return (
+    <div>
+      <span>{`${name}`}: </span>
+      <select value={currerntValue} name={`${name}`} onChange={onChange}>
+        {
+          data.map((value) => {
+            return (
+              <option value={value}>{value}</option>
+            )
+          })
+        }
+      </select>
+    </div>
+  );
 };
 
 export default DropDown;
